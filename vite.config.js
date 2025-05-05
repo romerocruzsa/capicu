f09c531482bb481ecce72683127c7f0cd975c371
romerocruzsa <sebastian.cruz6@upr.edu>
2025-05-05 14:11:27 -0400

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/capicu/', // MUST match your GitHub repo name
-  plugins: [react()],
-});
