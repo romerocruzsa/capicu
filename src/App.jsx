e07bd417b330ece024c20f30c5017de8ebdcda46
romerocruzsa <sebastian.cruz6@upr.edu>
2025-05-15 17:32:23 -0400

    fix: remove basename from BrowserRouter for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/capicu">
+    <BrowserRouter>
       <div className="app">
         <Navbar />
         <main>
